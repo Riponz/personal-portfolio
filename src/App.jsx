66a5015b7c86a30baf1e12597a7442aa55601f34
Diganta Biswas <51294318+Riponz@users.noreply.github.com>
2024-03-11 01:50:33 +0530

    under deployement msg update

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
 
   return (
     <div className='app-head'>
+      <div className="message fixed z-50 md:font-medium bg-white text-black text-lg md:text-sm flex justify-center items-center font-bold w-full h-15 p-5">This website is still under development! Your wait is appreciated.</div>
       <Navbar />
       <HeroText />
       <SelfBanner/>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
     }
     return (
         <>
-        <nav className='w-full z-50 bg-[#101010] fixed px-10 h-16 flex justify-between items-center'>
+        <nav className='w-full z-50 bg-[#101010] top-16 md:top-14 fixed px-10 h-16 flex justify-between items-center'>
             <span className='text-2xl text-red-600 hover:text-[#BCB5A8] md:text-lg md:font-normal font-bold'>diganta://</span>
             <div onClick={handleOpen} className='md:z-20 md:flex hidden md:justify-center'>{open? <CloseIcon/> : <MenuIcon className='text-2xl'/> }</div>
             <ol className={`w-[500px] md:block md:text-right md:fixed md:w-60 md:text-2xl md:p-5 md:right-0 flex justify-evenly md:bg-gray-400 md:bg-clip-padding md:backdrop-filter md:backdrop-blur-sm md:bg-opacity-10 items-center font-medium text-1xl md:transition-all md:duration-500 md:h-full z-10 md:ease-in ${open ? 'md:top-16 ':'top-[-1000px]'}`}>
