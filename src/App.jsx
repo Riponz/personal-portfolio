9f68021fb82044b1e80667ce024a6dc7fa924d2e
Diganta Biswas <51294318+Riponz@users.noreply.github.com>
2024-04-15 02:14:09 +0530

    minor border filx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className='w-full flex justify-center bg-[#101010] flex-col items-center'>
       <Navbar />
-        <div className="main overflow-hidden bg-[#101010] border-2 border-white w-full sm:w-[640px] md:w-[768px] lg:w-[1024px] xl:w-[1280px] min-[1440px]:w-[1440px]">
+        <div className="main overflow-hidden bg-[#101010] w-full sm:w-[640px] md:w-[768px] lg:w-[1024px] xl:w-[1280px] min-[1440px]:w-[1440px]">
           
       {/* <div className="message fixed z-50 md:font-medium bg-white text-black text-lg md:text-sm flex justify-center items-center font-bold w-full h-15 p-5">This website is still under development! Your wait is appreciated.</div> */}
       <HeroText/>
